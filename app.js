64b7afea3a952b6c7f009e35f94d5d627d27292e
Ajay-sen <ajaysen4736@gmail.com>
2024-07-24 10:53:43 +0530

    chaange in app.js

--- app.js
+++ app.js
@@ -97,7 +97,8 @@ app.use((req,res,next)=>{
     res.locals.success=req.flash("success");
     res.locals.error=req.flash("error");
     // console.log(res.locals.success);
-    res.locals.currUser=req.user || NULL;
+    res.locals.currUser=req.user;
+    // console.log(req.user);
     next();
 });
 
